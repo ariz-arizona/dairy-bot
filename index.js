7de4c9a11f1b2bb1ec333c915f7588fd3292aa1c
arizona <ar.ariz.arizona@gmail.com>
2019-12-25 00:37:45 +0300

    scene boilerplate

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const Scene = require('telegraf/scenes/base')
 const puppeteer = require('puppeteer');
 const Markup = require('telegraf/markup');
 const Extra = require('telegraf/extra');
+
 const url = 'http://www.diary.ru/api/';
+const login = 'dairy-bot';
+const password = '319992738';
+const urls = {
+wtf2019:'https://wtf-2019.diary.ru/'
+}
 
 const { leave } = Stage;
 const bot = new Telegraf(process.env.TOKEN)
@@ -17,20 +23,48 @@ bot.catch((err, ctx) => {
 });
 
 // Greeter scene
-const greeter = new Scene('greeter')
-greeter.enter((ctx) => ctx.reply('Hi'))
-greeter.leave((ctx) => ctx.reply('Bye'))
-greeter.hears(/hi/gi, leave())
-greeter.on('message', (ctx) => ctx.reply('Send `hi`'))
+const wtfScene = new Scene('wtfScene')
+let browser;
+wtfScene.enter((ctx) => {
+    ctx.reply('WTF Hi');
+    (async () => {
+        browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ],
+        });
+
+            const page = await browser.newPage();
+
+    await page.goto(urls.login)
+    await page.type('#user_login', login)
+    await page.type('#user_pass', password)
+    page.click('.btn[type="submit"]');
+    await page.waitForNavigation();
+     const result = await page.evaluate(() => {
+         return document.body.toString,split(0,400);
+     })
+    })();
+    ctx.reply(result);
+})
+wtfScene.leave((ctx) => {
+    ctx.reply('WTF Bye')
+    (async()=>{
+        await browser.close()
+    })()
+})
+wtfScene.hears(/hi/gi, leave())
+wtfScene.on('message', (ctx) => ctx.reply('Send `hi`'))
 
 // Create scene manager
 const stage = new Stage()
 stage.command('cancel', leave())
 
 // Scene registration
-stage.register(greeter)
+stage.register(wtfScene)
 
 bot.use(session())
 bot.use(stage.middleware())
-bot.command('greeter', (ctx) => ctx.scene.enter('greeter'))
+bot.command('wtf2019', (ctx) => ctx.scene.enter('wtfScene'))
 bot.startPolling()
