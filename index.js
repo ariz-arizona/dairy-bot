7c7e9fca50e335f5745cd60371b49de4cd83b299
arizona <ar.ariz.arizona@gmail.com>
2019-12-24 23:24:39 +0300

    scene boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
-const puppeteer = require('puppeteer');
 const Telegraf = require('telegraf');
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base')
+const puppeteer = require('puppeteer');
 const Markup = require('telegraf/markup');
 const Extra = require('telegraf/extra');
 
@@ -8,27 +11,27 @@ const url = 'http://www.diary.ru/api/';
 
 const bot = new Telegraf(process.env.TOKEN)
 
-
 bot.catch((err, ctx) => {
     console.log(`Ooops, ecountered an error for ${ctx.updateType}`, err);
     ctx.reply("ERROR! LOOK LOGS PLS")
-  });
-  
+});
 
-bot.start((ctx) => ctx.reply('Привет :)'))
+// Greeter scene
+const greeter = new Scene('greeter')
+greeter.enter((ctx) => ctx.reply('Hi'))
+greeter.leave((ctx) => ctx.reply('Bye'))
+greeter.hears(/hi/gi, leave())
+greeter.on('message', (ctx) => ctx.reply('Send `hi`'))
 
-bot.hears('hi', (ctx) => ctx.reply('HALLO!'));
+// Create scene manager
+const stage = new Stage()
+stage.command('cancel', leave())
 
-bot.hears('diary', ctx => {
-    axios.get(url, {
-        method: 'user.auth',
-        username: 'aarizona',
-        password: 'Adinfinitum1'
-    }).then(result => {
-        ctx.reply(JSON.stringify(result));
-    }).catch(error => {
-        ctx.reply(JSON.stringify(error));
-    })
-})
+// Scene registration
+stage.register(greeter)
 
-bot.launch()
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.use(session())
+bot.use(stage.middleware())
+bot.command('greeter', (ctx) => ctx.scene.enter('greeter'))
+bot.startPolling()
