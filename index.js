acc13be98e707cf42146697c639e68b293a6b0d5
arizona <ar.ariz.arizona@gmail.com>
2020-02-01 20:52:07 +0300

    add timeout to load

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ wtfScene.enter((ctx, initialState) => {
 
             ctx.reply("OPEN BROWSER");
             ctx.reply(`GO TO ${urls[ctx.scene.state.id || 'wtf2019']}?tags=`)
-            await page.goto(`${urls[ctx.scene.state.id || 'wtf2019']}?tags=`)
+            await page.goto(`${urls[ctx.scene.state.id || 'wtf2019']}?tags=`, { waitUntil: 'networkidle2', timeout: 60 })
             await page.waitForNavigation();
             await page.type('#user_login', login)
             await page.type('#user_pass', password)
