1706d358554281a2725b5cbd202ca0c7f1b78901
arizona <ar.ariz.arizona@gmail.com>
2020-02-01 22:15:11 +0300

    update puppeteer

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ const urls = {
     wtf2020: 'https://wtfk2020.diary.ru/'
 }
 const browserArgs = {
-    headless: false, args: [
+    headless: true, args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
-        '--disable-gpu',
         '--single-process'
     ],
 };
