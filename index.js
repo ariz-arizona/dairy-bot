bc9aa4b88640d14b523c7540f6699854594ac697
arizona <ar.ariz.arizona@gmail.com>
2020-02-01 22:47:35 +0300

    wait until domcontentloaded

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ wtfScene.enter((ctx, initialState) => {
             ctx.reply(`GO TO ${urls[ctx.scene.state.id || 'wtf2019']}?tags=`)
             await page.goto(
                 `${urls[ctx.scene.state.id || 'wtf2019']}?tags=`,
-                { waitUntil: 'networkidle2', timeout: 60000 }
+                { waitUntil: 'domcontentloaded', timeout: 60000 }
             )
             await page.waitForNavigation();
             await page.type('#user_login', login)
