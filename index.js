063437a480120b92cf0ed2be5c3e9716b88e8270
arizona <ar.ariz.arizona@gmail.com>
2020-02-03 22:04:48 +0300

    wait fo nav => wait for tag

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ wtfScene.hears(/^(c|C)\d{1,}/gi, ctx => {
                         data[i] = [];
                         ctx.reply(`GO TO ${link}`);
                         await page.goto(link);
-                        await page.waitForNavigation( { waitUntil: 'domcontentloaded' });
+                        await page.waitFor( '.LinkMore', { timeout: 3 * 1000 });
                         await page.evaluate(() => {
                             const items = document.querySelectorAll('.singlePost');
                             for (const post of items) {
