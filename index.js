0f34e4914bc125dc95f735172e975cd6bf0d4e20
arizona <ar.ariz.arizona@gmail.com>
2020-04-03 23:33:33 +0300

    test more than one page

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ wtfScene.hears(/^(c|C)\d{1,}/gi, ctx => {
                         linkList[j].push(link);
                         data[j] = [];
                         ctx.reply(`${Object.keys(links)[j].toUpperCase()} PAGE ${linkList[j].length}`);
-                        await page.goto(link, { waitUntil: "networkidle", timeout: 60000 })
+                        await page.goto(link, { waitUntil: "networkidle2", timeout: 60000 })
                         await page.waitForSelector(".single-post");
                         if (false) {
                         await page.evaluate(() => {
